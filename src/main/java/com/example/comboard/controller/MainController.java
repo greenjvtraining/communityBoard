1d0f82e48d8e6de722ba9b88a28eb9c8af222a18
greenjvtraining <greenjvtraining@gmail.com>
2024-05-02 17:45:59 +0900

    메인컨트롤러에 테스트 메소드 추가

--- src/main/java/com/example/comboard/controller/MainController.java
+++ src/main/java/com/example/comboard/controller/MainController.java
@@ -2,6 +2,7 @@ package com.example.comboard.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
@@ -9,5 +10,10 @@ public class MainController {
 	@RequestMapping("/")
 	public String root() {
 		return "index";
+	}
+	
+	@RequestMapping("/test")
+	public @ResponseBody String test() {
+		return "test.....";
 	}
 }
